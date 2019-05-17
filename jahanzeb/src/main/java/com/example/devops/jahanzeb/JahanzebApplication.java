19cfbfa6de1db14ce421063659557759f36a3198
Jahanzeb Jabbar <jahanzeb.me4u@gmail.com>
2019-05-18 07:23:42 +0800

    files added

---
+++ jahanzeb/src/main/java/com/example/devops/jahanzeb/JahanzebApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops.jahanzeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JahanzebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JahanzebApplication.class, args);
+	}
+
+}

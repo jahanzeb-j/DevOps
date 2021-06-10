5f9cc2fd5ba575ec78371c57320696d6ac727264
Jahanzeb Jabbar <jahanzeb.me4u@gmail.com>
2021-06-10 22:11:41 +0500

    created user rest-controller

--- src/main/java/com/example/devops/jahanzeb/controller/UserController.java
+++ src/main/java/com/example/devops/jahanzeb/controller/UserController.java
@@ -1,24 +1,32 @@
 package com.example.devops.jahanzeb.controller;
 
+import com.example.devops.jahanzeb.model.UserEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/")
-    public  String runindex(){
-        return "index.html";
-    }
 
-    @RequestMapping("/user")
-    @ResponseBody
-    String user()
+    @RequestMapping("/")
+    public String user()
     {
         return "Hello User !!";
+    }
+
+    @GetMapping("/new")
+    public ResponseEntity<?> newChange()
+    {
+        UserEntity user = new UserEntity();
+        user.setUsid("1");
+        user.setUsername("jazi");
+        user.setPassword("243nnsdjsdd32k432dsdhd");
+        return ResponseEntity.ok(user);
     }
 
 }

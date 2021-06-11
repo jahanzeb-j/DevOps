7e3732e605d039fac49aac4ecb3ae3e6c13e6d77
jahanzebj <jahanzeb.jabbar@conurets.com>
2021-06-11 11:34:38 +0500

    test added

--- src/test/java/com/example/devops/jahanzeb/JahanzebApplicationTests.java
+++ src/test/java/com/example/devops/jahanzeb/JahanzebApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JahanzebApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertEquals(4, 4);
 	}
 
 }
